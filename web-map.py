817f17b945d056a704ebe34e245bb5e9e29e1aeb
Sara Franklin <shternafranklin@gmail.com>
2019-06-23 10:47:26 -0400

    Update web-map.py
    Markers colors based on elevation

--- web-map.py
+++ web-map.py
@@ -14,15 +14,27 @@ Volcano name: <br>
 Height: %s m
 """
 
-#<h4>Volcano information:</h4>
-#Height: %s m
+#Create function before the expression
+def color_producer(elevation):
+    if elevation < 1000:
+       return 'purple'
+    elif 1000<=elevation<3000:
+        return 'orange'
+    else:
+        return 'red'
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=5, title="Mapbox Bright")
 fg = folium.FeatureGroup(name="My Map")
 
 #Need to go through each list 
 for lt, ln, el,name in zip(lat, lon, elev, name): #need to use zip because it will combine it as the same time
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon = folium.Icon(color = "red")))
+    fg.add_child(folium.Marker(location=[lt, ln], 
+        popup=folium.Popup(iframe), 
+ #adding different colors of markers based on elevation
+ #create a function that returns the str based on the elevation
+        icon = folium.Icon(color=color_producer(el))))
+
 
 map.add_child(fg)
 map.save("Map3.html")
